a9fce88db5aad05a8c2e1161d572e3db3b8623f9
bobo <bomimg1989@gmail.com>
2022-11-26 12:17:51 +0800

    initializer ransack and add filter

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,11 @@
 class BooksController < ApplicationController
     def index
         @search = Book.ransack(params[:q])
-        @pagy, @records = pagy(@search.result)
+        @pagy, @records = pagy(@search.result, items: params.fetch(:page_items, 20))
     end
 
     def search 
         index
-        redirect_to books_path(params.to_unsafe_hash)
+        redirect_to books_path(params.to_unsafe_hash.except(:authenticity_token,:action, :controller, :commit))
     end
 end
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,12 @@ module.exports = {
     rtl: false,
     prefix: "",
     darkTheme: "dark",
-    themes: ["light", "dark"]
+    themes: ["light", "dark"],
+    minWidth: {
+      '72': '18rem', /* 288px */
+      '80': '20rem', /* 320px */
+      '96': '24rem', /* 384px */
+    }
   },
   theme: {
     listStyleType: {
