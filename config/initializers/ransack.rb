525837b9e2d0549e8a9847731472dada1adbf6eb
jisuanjixue <2078350663@qq.com>
2022-12-02 09:30:53 +0800

    batch filter

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -1,83 +1,141 @@
 module Ransack
-    class Search
-        def build(params)
-            collapse_multiparameter_attributes!(params).each do |key, value|
-              if ['s'.freeze, 'sorts'.freeze].freeze.include?(key)
-                send("#{key}=", value)
-              elsif ['f'.freeze, 'fields'.freeze].freeze.include?(key)
-                send("#{key}=", value)
-              elsif @context.ransackable_scope?(key, @context.object)
-                add_scope(key, value)
-              elsif base.attribute_method?(key)
-                base.send("#{key}=", value)
-              elsif !Ransack.options[:ignore_unknown_conditions] || !@ignore_unknown_conditions
-                raise ArgumentError, "Invalid search term #{key}"
-              end
-            end
-            self
+  module Nodes
+    class Batch < Sort
+      attr_accessor :expanded
+
+      def build(params)
+        params.with_indifferent_access.each do |key, value|
+          send("#{key}=", value) if key.match(/^(name|dir|ransacker_args|expanded)$/)
         end
 
-        def default_fields=(args)
-          @default_fields = args.map { |field| Nodes::Attribute.new(@context, field) }
-        end
-    
-        def default_fields
-          @default_fields ||= []
-        end
-      
-          def fields=(args)
-            @fields ||= default_fields
-      
-            args.each do |field|
-              case field
-              when Hash
-                field = Nodes::Attribute.new(@context, field['name'])
-              when String
-                next if field == ''
-                field = Nodes::Attribute.new(@context, field)
-              end
-      
-              fields << field
-            end
-          end
-          alias f= fields=
-      
-          def fields
-            @fields ||= default_fields
-      
-            @fields
-          end
-          alias f fields
-      
-          def build_field(opts = {})
-            new_field(opts).tap do |field|
-              fields << field
-            end
-          end
-      
-          def new_field(opts = {})
-            Nodes::Attribute.new(@context, opts)
-          end
+        self
+      end
 
-          def field_attributes
-            fields.map(&:name).uniq
-          end
-
-          def hidden_fields
-            default_fields - fields
-          end
-
-          def batch_attribute
-            batch&.attr_name
-          end      
-          
-        def condition_attributes
-            groupings_attr_names = groupings.flat_map { |g| g.conditions.flat_map { |c| c.attributes.map(&:attr_name) } }
-            conditions_attr_names = conditions.flat_map { |c| c.attributes.map(&:attr_name) }
-            (groupings_attr_names + conditions_attr_names).uniq
-          end
-        def sort_attributes
-            sorts.map(&:attr_name).uniq
-        end
+      def expanded=(value)
+        @expanded = value == "true" ? true : false
+      end
     end
+  end
+
+  class Search
+    def build(params)
+      collapse_multiparameter_attributes!(params).each do |key, value|
+        if ["s".freeze, "sorts".freeze].freeze.include?(key)
+          send("#{key}=", value)
+        elsif ["f".freeze, "fields".freeze].freeze.include?(key)
+          send("#{key}=", value)
+        elsif ["b".freeze, "batch".freeze].freeze.include?(key)
+          send("#{key}=", value)
+        elsif @context.ransackable_scope?(key, @context.object)
+          add_scope(key, value)
+        elsif base.attribute_method?(key)
+          base.send("#{key}=", value)
+        elsif !Ransack.options[:ignore_unknown_conditions] || !@ignore_unknown_conditions
+          raise ArgumentError, "Invalid search term #{key}"
+        end
+      end
+      self
+    end
+
+    def batch=(args)
+      @batch ||= nil
+
+      case args
+      when Hash
+        @batch = Nodes::Batch.new(@context).build(args)
+      when Nodes::Batch
+        @batch = args
+      end
+    end
+
+    alias b= batch=
+
+    def batch
+      @batch ||= nil
+
+      @batch
+    end
+
+    alias b batch
+
+    def build_batch(opts = {})
+      new_batch(opts).tap do |batch|
+        self.batch = batch
+      end
+    end
+
+    def new_batch(opts = {})
+      Nodes::Batch.new(@context).build(opts)
+    end
+
+    def default_fields=(args)
+      @default_fields = args.map { |field| Nodes::Attribute.new(@context, field) }
+    end
+
+    def default_fields
+      @default_fields ||= []
+    end
+
+    def fields=(args)
+      @fields ||= default_fields
+
+      args.each do |field|
+        case field
+        when Hash
+          field = Nodes::Attribute.new(@context, field["name"])
+        when String
+          next if field == ""
+          field = Nodes::Attribute.new(@context, field)
+        end
+
+        fields << field
+      end
+    end
+
+    alias f= fields=
+
+    def fields
+      @fields ||= default_fields
+
+      @fields
+    end
+
+    alias f fields
+
+    def build_field(opts = {})
+      new_field(opts).tap do |field|
+        fields << field
+      end
+    end
+
+    def new_field(opts = {})
+      Nodes::Attribute.new(@context, opts)
+    end
+
+    def field_attributes
+      fields.map(&:name).uniq
+    end
+
+    def hidden_fields
+      default_fields - fields
+    end
+
+    def batch_attribute
+      batch&.attr_name
+    end
+
+    def condition_attributes
+      groupings_attr_names = groupings.flat_map { |g| g.conditions.flat_map { |c| c.attributes.map(&:attr_name) } }
+      conditions_attr_names = conditions.flat_map { |c| c.attributes.map(&:attr_name) }
+      (groupings_attr_names + conditions_attr_names).uniq
+    end
+
+    def sort_attributes
+      sorts.map(&:attr_name).uniq
+    end
+
+    def batch_attribute
+      batch.attr_name
+    end
+  end
 end
