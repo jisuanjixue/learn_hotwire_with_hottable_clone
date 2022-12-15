f3c250fe3fad6c06fa0b5d189e02dcb9727012d2
bobo <bomimg1989@gmail.com>
2022-12-15 20:08:18 +0800

    fix checkout stimulus and add icon component

--- app/components/application_component.rb
+++ app/components/application_component.rb
@@ -32,4 +32,40 @@ class ApplicationComponent < Phlex::HTML
       tokens.compact.join(" ")
     end
   end
+
+  def attributify(*attribute_hashes)
+    flat_attribute_hashes = attribute_hashes.map { flatten_attributes_hash(_1) }
+
+    flat_attribute_hashes.reduce({}) do |memo, attribute_hash|
+      memo.deep_merge(attribute_hash) do |attribute, oldval, newval|
+        next newval unless ["class", "data-controller", "data-action"].include?(attribute)
+        next newval if attribute.end_with?("!")
+
+        [oldval, newval].uniq.join(" ")
+      end
+    end
+  end
+
+  def flatten_attributes_hash(input, keys = [], output = {})
+    return output.merge!(keys.join("-") => input) unless input.is_a?(Hash)
+
+    input.each do |key, value|
+      name = case key
+        when String
+          key
+        when Symbol
+          key.name.tr("_", "-")
+        else
+          key.to_s
+        end
+
+      flatten_attributes_hash(
+        value,
+        keys + Array[name],
+        output
+      )
+    end
+
+    output
+  end
 end
